772b546efd43b4726e5321632a6e4146e6ad8a7d
Ryan K Burns <ryankburns@gmail.com>
2020-09-22 16:16:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@
 def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
+
+def batch_badge_creator(array)
+  badge_messege_array = []
+  array.each do |name|
+    badge_messege_array >> "Hello, my name is #{name}"
+  end
+end
